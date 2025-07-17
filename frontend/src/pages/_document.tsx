19e0af78c5c5ee06ab3b9a382b62aac1695e094c
Iheb Chaoued <ihebchaoued@gmail.com>
2025-07-17 17:13:30 +0100

    Dark Mode option Added

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const setInitialTheme = `
+    (function () {
+      try {
+        const theme = localStorage.getItem('theme');
+        if (theme === 'dark') {
+          document.documentElement.classList.add('dark');
+        } else {
+          document.documentElement.classList.remove('dark');
+        }
+      } catch (_) {}
+    })();
+  `;
+
   return (
     <Html lang="en">
       <Head />
       <body>
+        <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
         <Main />
         <NextScript />
       </body>
